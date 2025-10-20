17cbb687041807e48691b913bb3db1e2930c8da7
Robin Quast <robin.quast@web.de>
2025-10-20 09:06:35 +0000

    added about page, login page and everything is reachable through the navigation bar at the top (+ some basic css)

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const app = express();
 app.set('view engine', 'pug');      // tell Express to use Pug
 app.set('views', './views');        // optional: path to your templates folder
 
+// Serve static assets (CSS/JS)
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+
 app.get('/', (req, res) => {
   res.render('index', { title: 'Meine Pug-Seite' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'Über uns' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Kontakt' });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', { title: 'Einloggen' });
 });
 
 app.listen(3000, () => console.log('Server läuft auf http://localhost:3000'));
